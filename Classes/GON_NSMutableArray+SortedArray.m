18cce10104618efdd0bd944bf0b4e76a8dc8d742
nicolasgoutaland <nicolas.goutaland@gmail.com>
2015-04-07 17:52:37 +0200

    Fixed categories bugs

--- Classes/GON_NSMutableArray+SortedArray.m
+++ Classes/GON_NSMutableArray+SortedArray.m
@@ -11,7 +11,7 @@
 @implementation NSMutableArray(SortedArray)
 #pragma mark - SortedArray
 static NSComparisonResult cw_SelectorCompare(id a, id b, void* selector) {
-    return (NSComparisonResult)(((id(*)(id, SEL, id))objc_msgSend)(a, (SEL)aSelector, b));
+    return (NSComparisonResult)(((id(*)(id, SEL, id))objc_msgSend)(a, (SEL)selector, b));
 }
 
 - (void)insertObject:(id)object sortedUsingFunction:(NSInteger (*)(id, id, void *))compare context:(void*)context;
@@ -20,21 +20,21 @@ static NSComparisonResult cw_SelectorCompare(id a, id b, void* selector) {
 	[self insertObject:object atIndex:index];
 }
 
-- (void)insertObject:(id)object sortedUsingSelector:(SEL)aSelector;
+- (void)insertObject:(id)object sortedUsingSelector:(SEL)selector
 {
-	NSUInteger index = [self indexForInsertingObject:anObject sortedUsingFunction:&cw_SelectorCompare context:selector];
+	NSUInteger index = [self indexForInsertingObject:object sortedUsingFunction:&cw_SelectorCompare context:selector];
 	[self insertObject:object atIndex:index];
 }
 
-- (void)insertObject:(id)object sortedUsingDescriptors:(NSArray*)descriptors;
+- (void)insertObject:(id)object sortedUsingDescriptors:(NSArray*)descriptors
 {
-	NSUInteger index = [self indexForInsertingObject:anObject sortedUsingDescriptors:descriptors];
+	NSUInteger index = [self indexForInsertingObject:object sortedUsingDescriptors:descriptors];
 	[self insertObject:object atIndex:index];
 }
 
 - (void)insertObject:(id)object sortedUsingBlock:(NSComparisonResult (^)(id obj1, id obj2))comparatorBlock
 {
-	NSUInteger index = [self indexForInsertingObject:anObject sortedUsingBlock:comparatorBlock];
+	NSUInteger index = [self indexForInsertingObject:object sortedUsingBlock:comparatorBlock];
 	[self insertObject:object atIndex:index];
 }
 
